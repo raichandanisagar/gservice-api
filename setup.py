eedde00954de937318155e8b25e8ade086706c87
raichandanisagar <raichandanisagar@gmail.com>
2021-11-25 00:03:32 +0530

    Updated package

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: sagarraichandani
+"""
+
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='toolbox',
+    version='0.0.1',
+    author='Sagar Raichandani',
+    author_email='raichandanisagar@gmail.com',
+    description='Python package that makes working with Google APIs simpler.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/raichandanisagar/gservice-api',
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['google-api-core','google-api-python-client','google-auth',
+                      'google-auth-httplib2','google-auth-oauthlib','googleapis-common-protos',
+                      'pandas']
+    
+)
